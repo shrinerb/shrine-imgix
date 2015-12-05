2ee6fe2bdafe7dd58e0239602b3223783b3a8a8d
Janko Marohnić <janko.marohnic@gmail.com>
2015-12-05 23:36:30 +0100

    Remove unnecessary requires from test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,8 @@ require "minitest/autorun"
 require "minitest/pride"
 
 require "shrine/storage/imgix"
-
-require "forwardable"
-require "stringio"
-
 require "dotenv"
+
 Dotenv.load!
 
 class Minitest::Test
